b8fbeb672abe6c8ae2207ae5590b6c958b0f1bb6
Brandon Endres <17bendres@gmail.com>
2021-04-02 18:23:43 -0400

    Adds web-cam stream functionality, face detection

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -11,19 +11,11 @@ import WebCam from "./WebCam.svelte";
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 516px;
 	}
 
 	h1 {
 		color: var(--primary-color);
-		font-size: 4em;
 		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		margin: 0;
 	}
 </style>
--- client/src/WebCam.svelte
+++ client/src/WebCam.svelte
@@ -1,18 +1,40 @@
 <script lang="ts">
-import { onMount } from "svelte";
+    import { onMount } from "svelte";
+    import * as faceapi from 'face-api.js';
 
-
-    const video = document.querySelector("#videoEl") as HTMLMediaElement;
+    const detectAllFaces = async (video, canvas) => {
+        const allFaces = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors();
+        faceapi.matchDimensions(canvas, { width:650, height:450});
+        const fullFaceDescription = faceapi.resizeResults(allFaces, {width:650, height:450})
+        faceapi.draw.drawDetections(canvas, fullFaceDescription);
+    }
 
     onMount(async () => {
-        if (navigator.mediaDevices.getUserMedia) {
-           const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-           video.srcObject = stream;
+        try {
+            await faceapi.loadFaceLandmarkModel("../../models");
+            await faceapi.loadFaceRecognitionModel("../../models");
+            await faceapi.loadTinyFaceDetectorModel("../../models");
+
+            const canvas = document.querySelector("#canvas");
+            const video: HTMLMediaElement = document.querySelector("#videoEl") as HTMLMediaElement;
+            if (navigator.mediaDevices.getUserMedia) {
+                const stream: MediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+                video.srcObject = stream;
+            } else {
+                // TODO: Display message saying no media devices found
+            }
+            
+            setInterval(async () => {
+                await detectAllFaces(video, canvas);
+            }, 300);
+        } catch (error) {
+            console.error(error);
         }
-    })
+    });
 </script>
 
 <main>
+    <canvas id="canvas" class="overlay"></canvas>
     <video autoplay={true} id="videoEl"> 
         <track kind="captions">
     </video>
@@ -20,18 +42,21 @@ import { onMount } from "svelte";
 
 <style>
     main {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 516px;
-        height: 391px;
+        max-width: 600px;
+        height: 450px;
         border: 2px solid var(--primary-color);
-        padding: 1rem;
+        padding: 0.5rem;
+    }
+
+    .overlay {
+        position: absolute;
+        width: 600px;
+        height: 450px;
     }
 
     video {
-        width: 500px;
-        height: 375px;
+        width: 600px;
+        height: 450px;
         background-color: var(--text-color);
     }
 </style>
