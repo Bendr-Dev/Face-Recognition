4d4807a98d43c4dbca3638211d4e5034a09ff743
Brandon Endres <17bendres@gmail.com>
2021-04-01 21:16:45 -0400

    Creates boilerplate

---
+++ client/src/App.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+</script>
+
+<main>
+	<h1>Face Recognition</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: var(--primary-color);
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
